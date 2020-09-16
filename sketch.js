2a4f2ab8ab2ea01855fdfb6450de0b78387435c3
camillacarella <69818491+camillacarella@users.noreply.github.com>
2020-09-16 02:24:37 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,9 @@
-
-let sketch;
-
 function setup() {
-  sketch = createCanvas(700, 400);
-  sketch.parent("p5");
+  createCanvas(700, 400);
 }
 
 function draw() {
-  
-  // define color aleatorio 
   stroke(random(255), random(255), random(255));
-  // defino un grosor aleatorio
-  strokeWeight(random(2, 20));
-  // dibujo un punto aleatorio
-  point(random(10, width-10), random(10, height - 10));
-  
+  strokeweight(random(2, 20));
+  point(random(width), random(height));
 }
-
